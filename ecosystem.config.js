9f655bb35c5c3ee69482701628cb0632d764d724
Administrador <admin@MacBook-Pro-de-Admin.local>
2023-03-16 23:47:57 -0300

    Adicionado o arquivo ecosystem.config

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'WEB-WHATS-API',
+      script: 'dist/main.js',
+      instances: 1,
+      exec_mode: 'cluster',
+      autorestart: true,
+      watch: false,
+      //max_memory_restart: '2G',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
